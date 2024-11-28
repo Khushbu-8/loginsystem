cba69bc2efc0ba4dceb764d2cf7c5316ba940446
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-11-28 21:12:54 +0530

    api

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
                         <input
                             type="text"
                             id="phone"
-                            value={email}
+                            value={phone}
                             onChange={(e) => setPhone(e.target.value)}
                             required
                             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
